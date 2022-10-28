91fbfb95c349a2f29e02473c64f721e5035656f9
Geoffrey <gcloud@uncc.edu>
2022-10-28 11:07:42 -0400

    saving note to mock list of notes

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -5,8 +5,13 @@ import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
+from flask import redirect, url_for 
 
 app = Flask(__name__)     # create an app
+notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 
+             2: {'title':'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
+             3: {'title':'Third note', 'text': 'This is my Third note', 'date': '10-3-2020'}}
+
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -21,31 +26,37 @@ def index():
 def get_notes():
     a_user = {'name': 'Geoffrey', 'email':'gcloud@uncc.edu'}
 
-    notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 
-             2: {'title':'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title':'Third note', 'text': 'This is my Third note', 'date': '10-3-2020'}
-             }
-
     return render_template('notes.html', notes = notes, user = a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     a_user = {'name': 'Geoffrey', 'email':'gcloud@uncc.edu'}
 
-    notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'}, 
-             2: {'title':'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
-             3: {'title':'Third note', 'text': 'This is my Third note', 'date': '10-3-2020'}}
-
+    
     return render_template('note.html', note = notes[int(note_id)], user = a_user)
 
 @app.route('/notes/new', methods = ['GET', 'POST'])
 def new_note():
+    # mock user
     a_user = {'name': 'Geoffrey', 'email': 'gcloud@gmail.com'}
 
-    print('request method is ', request.method)
+    #check if method is used for request
     if (request.method == 'POST'):
-        request_data = request.form
-        return f"data: {request_data} !"
+        #get title
+        title = request.form['title']
+        # get note
+        text = request.form['noteText']
+        #create date stamp
+        from datetime import date
+        today = date.today();
+        #format mm//dd//yyyy
+        today = today.strftime("%m-%d-%Y")
+        #get the last ID used and incremement by 1
+        id = len(notes)+1
+        #create new note entry
+        notes[id] = {'title': title, 'text': text, 'date': today}
+        #ready to render response - redirect to notes listing
+        return redirect(url_for('get_notes'))
     else:
         return render_template('new.html', user = a_user)
 
